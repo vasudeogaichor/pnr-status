6f4122c01076af456cc202302b5db1358f2143cf
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-03-06 22:48:55 +0530

    login page init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import {PNRHome} from './home';
+import  { LoginContainer } from './components/login'
+import { Route, Router } from 'React';
+
+
 
 function App() {
   return (
+    <Router>
+      <Route exact path='/login' element={< LoginContainer />}></Route>
     <div className="App">
       <PNRHome/>
     </div>
+    </Router>
   );
 }
 
